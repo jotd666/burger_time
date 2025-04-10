f1878491a162ee6f5e2a927a652da1e54df2ce73
Jean-François Fabre <jotd666@gmail.com>
2025-04-10 23:20:19 +0200

    maps extract working

--- assets/amiga/dump_backgrounds.py
+++ assets/amiga/dump_backgrounds.py
@@ -1,9 +1,36 @@
 from PIL import Image
-import pathlib
+import pathlib,os
 
 with open("ab03.6b","rb") as f:
     contents = f.read()
 
-dumps_dir = pathlib.Path("dumps/background")
+bgpic = pathlib.Path("../sheets/background.png")
 
-bg_tiles = {i:Image.open(dumps_dir / f"unknown_{i:03x}.png") for i in range(0,0xd)}
+bg_image = Image.open(bgpic)
+
+bg_tiles = []
+x = 0
+for i in range(0,0xe):
+    img = Image.new("RGB",(16,16))
+    img.paste(bg_image,(-x,0))
+    x += 16
+    bg_tiles.append(img)
+
+img = Image.new("RGB",(256,256))
+
+icont = iter(contents)
+
+pathlib.Path("dumps/maps").mkdir(exist_ok=True)
+
+# there are 6 different level layouts
+for lvl in range(0,6):
+    y = 0
+    for i in range(0,16):
+        x = 0
+        for j in range(0,16):
+            c = next(icont)
+            img.paste(bg_tiles[c],box=(x,y))
+            x += 16
+        y += 16
+
+    img.save(f"dumps/maps/level_{lvl+1}.png")
