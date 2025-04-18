ebf2097b2a70d2063c5791668613da398942fed3
Jean-François Fabre <jotd666@gmail.com>
2025-04-18 22:31:11 +0200

    trying to create a floppy

---
+++ README.md
@@ -0,0 +1,25 @@
+# Burger Time
+transcode of Burger Time arcade version for 68000/020 amiga machines
+
+Credits:
+
+- jotd: 6502 to 68000 conversion, amiga graphics conversion, amiga sound effects
+- PascalDe73: icon
+
+Highscores & best times are saved (unless cheat is on)
+
+Controls:
+
+- joystick left/right/up/down: move character
+- joystick fire: pepper
+
+To start a game (when game not running):
+
+- insert coin: fire/5
+- start game (1 player): up/1
+- start game (2 players): down/2
+
+Cheat keys:
+- F1: complete level
+- F3: add 10000 points
+- F4: toggle invincibility
---
+++ tools/create_amiga_archive.py
@@ -0,0 +1,39 @@
+import subprocess,os,glob,shutil
+
+progdir = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
+
+gamename = "burger_time"
+# JOTD path for cranker, adapt to wh :)
+os.environ["PATH"] += os.pathsep+r"K:\progs\cli"
+
+cmd_prefix = ["make","-f",os.path.join(progdir,"makefile.am")]
+
+subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
+
+for s in ["convert_sounds.py","convert_graphics.py"]:
+    subprocess.check_call(["cmd","/c",s],cwd=os.path.join(progdir,"assets","amiga"))
+
+subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd=os.path.join(progdir,"src"))
+# create archive
+
+outdir = os.path.join(progdir,f"{gamename}_HD")
+
+if os.path.exists(outdir):
+    for x in glob.glob(os.path.join(outdir,"*")):
+        os.remove(x)
+else:
+    os.mkdir(outdir)
+for file in ["readme.md",f"{gamename}.slave"]:
+    shutil.copy(os.path.join(progdir,file),outdir)
+
+shutil.copy(os.path.join(progdir,"assets","amiga","BurgerTime.info"),outdir)
+
+
+for suffix in ["000","020"]:
+    # pack the file for floppy
+    exename = f"{gamename}_{suffix}"
+    shutil.copy(os.path.join(progdir,exename),outdir)
+    subprocess.check_output(["cranker_windows.exe","-f",os.path.join(progdir,exename),"-o",os.path.join(progdir,f"{exename}.rnc")])
+
+subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
+
